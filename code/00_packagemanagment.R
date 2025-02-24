06adc547b1cfd03a84402d0a2ebb95f1ca6c7f49
Ruben Dewitte <ruben0dewitte@gmail.com>
2025-02-24 20:40:47 +0100

    Yearly update

--- code/00_packagemanagment.R
+++ code/00_packagemanagment.R
@@ -2,6 +2,6 @@
 
 install.packages("here", version='1.0.1')
 install.packages("tidyverse", version='2.0.0')
-install.packages("fixest", version='0.11.1')
-install.packages("huxtable", version='5.5.2')
-install.packages("flextable", version='0.8.6')
+install.packages("fixest", version='0.12.1')
+install.packages("huxtable", version='5.5.7')
+install.packages("flextable", version='0.9.7')
